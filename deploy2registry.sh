7ec7fc698777b4caf32e3d1e54893939957584cc
Joao Lucas <joaollr86@gmail.com>
2022-08-30 07:16:33 -0300

    Included deploy2registry.sh

---
+++ deploy2registry.sh
@@ -0,0 +1,11 @@
+echo 'DEPLOY TO REGISTRY'
+docker login --username $DOCKERHUB_USERNAME --password $DOCKERHUB_PASSWORD
+docker build -f Dockerfile -t $SERVICE .
+docker tag $SERVICE $DOCKERHUB_USERNAME/$SERVICE:dev
+docker push $DOCKERHUB_USERNAME/$SERVICE:dev
+echo 'DEPLOY TO KUBERNETES'
+curl 'https://raw.githubusercontent.com/joaollr86/egeo_architecture/main/'$SERVICE'_deploy.yml' > deploy_base.yml
+cat deploy_base.yml
+apt-get install gettext-base
+envsubst < deploy_base.yml > deploy.yml
+cat deploy.yml
