8c35a7b6d5446219a17c6e56d87459373160e53b
Joao Lucas <joaollr86@gmail.com>
2022-08-30 07:41:35 -0300

    Correction in deploy2registry.sh

--- deploy2registry.sh
+++ deploy2registry.sh
@@ -1,8 +1,8 @@
 echo 'DEPLOY TO REGISTRY'
 docker login --username $DOCKERHUB_USERNAME --password $DOCKERHUB_PASSWORD
 docker build -f Dockerfile -t $SERVICE .
-docker tag $SERVICE $DOCKERHUB_USERNAME/$SERVICE:dev
-docker push $DOCKERHUB_USERNAME/$SERVICE:dev
+docker tag $SERVICE $DOCKERHUB_USERNAME/$SERVICE:$VERSION
+docker push $DOCKERHUB_USERNAME/$SERVICE:$VERSION
 echo 'DEPLOY TO KUBERNETES'
 curl 'https://raw.githubusercontent.com/joaollr86/egeo_architecture/main/'$SERVICE'_deploy.yml' > deploy_base.yml
 cat deploy_base.yml
